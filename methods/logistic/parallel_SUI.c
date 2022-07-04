aeb549eac6fa10f7ad247d5418346366d00979b5
Lidija Fodor <lidija.fodor@dmi.uns.ac.rs>
2022-07-04 19:57:34 +0200

    Update parallel_SUI.c

--- methods/logistic/parallel_SUI.c
+++ methods/logistic/parallel_SUI.c
@@ -4,6 +4,11 @@
  *  Created on: Oct 10, 2016
  *      Author: lidija
  */
+#include "util.utility.h"
+#include "io/read_write.h"
+#include "neighbourhood/neighbourhood_check.h"
+#include "compute_updates/comp_upd.h"
+
 #include <lapacke.h>
 #include <stdio.h>
 #include <lapacke_utils.h>
@@ -18,176 +23,7 @@
 #include <stdlib.h>
 #include<time.h>
 
-double max(double* a, int Dim);
-double min(double* a, int Dim);
-int minInt(int* a, int Dim);
-void printVector(double* vector, int n, char* name, int id);
-void DQNquadraticParallel(int N, int Dim, int size_row, int size_col, char* type, char* path);
-int get_my_active_neighbour(int k, int my_rank, int *my_neighbours, int my_neighbours_count, int *active);
-
-int main(int argc ,char* argv[]) {
-	if(argc<7){
-		printf("Input parameters N, Dim, size_row and size_col not specified.");
-		return -1;
-	}
-	int N=atoi(argv[1]);
-	int Dim=atoi(argv[2]);
-	int size_row=atoi(argv[3]);
-	int size_col=atoi(argv[4]);
-	char* type=argv[5];
-	char* path=argv[6];
-	DQNquadraticParallel(N, Dim, size_row, size_col, type, path);
-	return 0;
-}
-
-int get_my_active_neighbour(int k, int my_rank, int *my_neighbours, int my_neighbours_count, int *active){
-	int cnt_active=-1;
-	int neighbour=-1;
-	for(int i=0;i<my_neighbours_count;i++){
-		if(active[my_neighbours[i]]){
-			++cnt_active;
-			neighbour=my_neighbours[i];
-		}
-		if(cnt_active==k)
-			return neighbour;
-	}
-}
-
-void get_my_data(double* A, double *B, double *WMatrix, double *myWMatrix, double *stepSize, int my_rank, int N, int Dim, int N_f, int N_w, int Nx, double lambda_penal,
-					 int* rem, char* type, char* path){
-	double *Adata=calloc(N_f*N_w*N+*rem*N_w, sizeof(double));
-	double *Bdata=calloc(N_f*N+*rem, sizeof(double));
-	int *Adj=calloc(N*N, sizeof(double));
-	int *degreeSensor=calloc(N, sizeof(double));
-
-	if(my_rank==0){
-		int fd;
-		char infile[50];
-		strcpy(infile, type);
-		strcat(infile, "/Adata.bin");
-		printf("%s\n", infile);
-		int bytes_expected=(N_f*N_w*N+*rem*N_w)*sizeof(double);
-		fd=open(infile, O_RDONLY);
-		read(fd, Adata, bytes_expected);
-
-		strcpy(infile, type);
-		strcat(infile, "/Bdata.bin");
-		printf("%s\n", infile);
-		bytes_expected=(N_f*N+*rem)*sizeof(double);
-		fd=open(infile, O_RDONLY);
-		read(fd, Bdata, bytes_expected);
-
-		strcpy(infile, path);
-		strcat(infile, "/Adj.bin");
-		printf("%s\n", infile);
-		bytes_expected = N * N * sizeof(int);
-		fd=open(infile, O_RDONLY);
-		read(fd, Adj, bytes_expected);
-
-		strcpy(infile, path);
-		strcat(infile, "/degSens.bin");
-		printf("%s\n", infile);
-		bytes_expected = N * sizeof(int);
-		fd=open(infile, O_RDONLY);
-		read(fd, degreeSensor, bytes_expected);
-	}
-
-	if(*rem!=0){
-		int *sendcounts=calloc(N, sizeof(int));
-		int *displs=calloc(N, sizeof(int));
-		sendcounts[0]=N_w*(N_f+*rem);
-		displs[0]=0;
-		for(int p=1;p<N;p++){
-			sendcounts[p]=N_w*N_f;
-			displs[p]=p*N_w*N_f+N_w**rem;		
-		}
-		MPI_Scatterv(Adata, sendcounts, displs, MPI_DOUBLE, A, sendcounts[my_rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		sendcounts[0]=N_f+*rem;
-		displs[0]=0;
-		for(int p=1;p<N;p++){
-			sendcounts[p]=N_f;
-			displs[p]=N_f*p+*rem;
-		}
-		MPI_Scatterv(Bdata, sendcounts, displs, MPI_DOUBLE, B, sendcounts[my_rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	}
-	else{
-		MPI_Scatter(Adata, N_f*N_w, MPI_DOUBLE, A, N_f*N_w, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		MPI_Scatter(Bdata, N_f, MPI_DOUBLE, B, N_f, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-	}
-	free(Adata);
-	free(Bdata);
-
-	int i,j;
-	if(my_rank==0){
-		for(i=N-2;i>=0;--i){
-			for(j=N-1;j>=i+1;--j){
-				if(Adj[i*N+j]==1){
-					WMatrix[i*N+j] = 1.0 / (1.0 + MAX(degreeSensor[i], degreeSensor[j]));
-					WMatrix[j*N+i] = WMatrix[i*N+j];
-				}
-			}
-		}
-
-		double *eye=calloc(N*N,sizeof(double));
-		double ones[N];
-		for(i=N-1;i>=0;--i){
-			eye[i*N+i]=1.0;
-			ones[i]=1.0;
-		}
-
-		double *v=calloc(N, sizeof(double));
-		cblas_dgemv(CblasRowMajor, CblasNoTrans, N, N, 1, WMatrix, N, ones, 1, 0, v, 1);
-		double *diagMatr=calloc(N*N, sizeof(double));
-		for(i=N-1;i>=0;--i)
-			diagMatr[i*N+i]=v[i];
-		cblas_daxpy(N*N, -1,diagMatr, 1, eye, 1);
-		cblas_daxpy(N*N, 1,eye, 1, WMatrix, 1);
-
-		double *eyeScaled=calloc(N*N, sizeof(double));
-		for(i=N-1;i>=0;--i){
-			eyeScaled[i*N+i]=0.5;
-		}
-		for(i=N*N-1;i>=0;--i)
-			WMatrix[i]*=0.5;
-
-		cblas_daxpy(N*N, 1, eyeScaled, 1, WMatrix, 1);
-
-		double *WMdiag=calloc(N, sizeof(double));
-		for(int j=0;j<N;j++)
-			WMdiag[j]=WMatrix[j*N+j];
-
-		free(eye);
-		free(eyeScaled);
-		free(diagMatr);
-		free(v);
-		free(WMdiag);
-	}
-	MPI_Scatter(WMatrix, N, MPI_DOUBLE, myWMatrix, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-	free(Adj);
-	free(degreeSensor);
-}
-
-void find_optimal_distribution(int *N_f, int *rem, int size_row, int N){
-	if(size_row % N ==0){
-		*N_f=size_row/N;
-		*rem=0;
-	}
-	else{
-		*N_f=size_row / N;
-		*rem=size_row % N;
-	}
-
-}
-
-int is_my_neighbour(int my_rank, int i, int *my_neighbours, int my_neighbours_count){
-	for(int j=my_neighbours_count-1;j>=0;j--)
-		if(my_neighbours[j]==i)
-			return 1;
-	return 0;
-}
-
-void DQNquadraticParallel(int N, int Dim, int size_row, int size_col, char* type, char* path){
+void DQNParallel_SUI(int N, int Dim, int size_row, int size_col, char* type, char* path){
 	int my_rank;
 	int procs;
 	int N_w=size_col;
@@ -290,8 +126,6 @@ void DQNquadraticParallel(int N, int Dim, int size_row, int size_col, char* type
 	   	AWeightInv[i*Dim+i] = 1.0;
 
 	double *GradOld=calloc(Dim, sizeof(double));
-
-	double local_communication_time_end, local_communication_time_start, local_comm_time=0.0;
 	int iters=0;
 
 	int stop=-1;
@@ -358,51 +192,10 @@ void DQNquadraticParallel(int N, int Dim, int size_row, int size_col, char* type
 		double *sDirection=calloc(Dim, sizeof(double));
 		int *pivotArray=calloc(Dim, sizeof(int));
 
-		//GRADIENT*************************************
+		compute_Gradient(N_w, N_x, N_f, rem, Dim, X, Gradient, GradOld, my_rank, Adata, Bdata, lambda_penal);
 
-		double *Sum=calloc(N_w+1, sizeof(double));
-		vv = X[Dim - 1];
-		LAPACKE_dlacpy(LAPACK_ROW_MAJOR, 'A', 1, Dim-1, X, Dim-1, ww, Dim-1);
-		LAPACKE_dlacpy(LAPACK_ROW_MAJOR, 'A', 1, Dim, Gradijent, Dim, GradOld, Dim);
+		compute_Hessian(Dim, N_f, N_w, N_x, my_rank, rem, Adata, ww, vv, lambda_penal, GMatrix, eye)
 
-		for(l=N_f+(my_rank==0)*rem-1; l >= 0; l--){
-			dot=cblas_ddot(Dim-1, Adata+(l * (N_w)), 1, ww, 1);
-			coeff =(dot + vv)* (-Bdata[l]);
-			coeff2 = exp(coeff) / (1 + exp(coeff));
-			
-			for (h = Nx-2; h >= 0; h--)
-				subMatr[h] = Adata[l*N_w+h] * (-Bdata[l]);
-			subMatr[Nx-1] = -Bdata[l];
-			cblas_daxpy(Nx, coeff2, subMatr, 1, Sum, 1);
-		}
-		LAPACKE_dlacpy( LAPACK_ROW_MAJOR, 'A', 1, Dim, Sum, Dim, Gradijent, Dim);
-		cblas_daxpy(Dim, lambda_penal, X, 1, Gradijent, 1);
-		free(Sum);
-
-		//GRADIENT************************************
-
-		//HESIAN**************************************
-
-		double *SumaMatrix = calloc(Dim*Dim, sizeof(double));
-		for (l = N_f+(my_rank==0)*rem-1; l >= 0; l--) {
-		 	dot=cblas_ddot(Dim-1, Adata+(l * N_w), 1, ww, 1);
-		 	coeff = (dot + vv) * (-Bdata[l]);
-		 	coeff2 = exp(coeff) / (1 + exp(coeff))/(1+exp(coeff));
-		 	double *tmp=calloc(Nx*Nx, sizeof(double));
-		 	for (h = Nx-2; h >= 0; h--)
-		 		subMatr[h] = Adata[l * N_w+h] * (-Bdata[l]);
-		 	subMatr[Nx-1] = (-Bdata[l]);
-		 	cblas_dger(CblasRowMajor, Nx, Nx, 1.0, subMatr, 1, subMatr, 1, tmp, Nx);
-		 	cblas_daxpy(Nx*Nx, coeff2, tmp, 1, SumaMatrix, 1);
-		 	free(tmp);
-		 }
-		 LAPACKE_dlacpy( LAPACK_ROW_MAJOR, 'A', Dim, Dim, SumaMatrix, Dim, GMatrix, Dim);
-		 free(SumaMatrix);
-		 cblas_daxpy(Dim*Dim, lambda_penal, eye, 1, GMatrix, 1);
-		
-		//HESIAN**************************************
-
-		//EXCHANGE**************************************
 
 		int my_communicator_size=0;
 		MPI_Comm_size(allCurrComms[my_rank], &my_communicator_size);
@@ -428,12 +221,11 @@ void DQNquadraticParallel(int N, int Dim, int size_row, int size_col, char* type
 		    	free(zero);
 	   	}
 	    	
-	  //EXCHANGE************************************
 	
 		double *curRes=calloc(Dim, sizeof(double));
-	  double *GradijentGlob=calloc(Dim*N, sizeof(double));
+	  	double *GradijentGlob=calloc(Dim*N, sizeof(double));
 
-	   double my_self_confidence=1-active_neighbours_weight;
+	   	double my_self_confidence=1-active_neighbours_weight;
 	 
 	   	for(i=0;i<Dim;i++){
 	   		curRes[i]=(1-my_self_confidence)*X[i];
@@ -510,9 +302,6 @@ void DQNquadraticParallel(int N, int Dim, int size_row, int size_col, char* type
 	double max_time;
 	MPI_Reduce(&elapsed, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-	double *comm_times=calloc(N, sizeof(double));
-	MPI_Gather(&local_comm_time, 1, MPI_DOUBLE, comm_times, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
 	double *Xall=calloc(Dim*N, sizeof(double));
 	MPI_Gather(X, Dim, MPI_DOUBLE, Xall, Dim, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
@@ -522,60 +311,15 @@ void DQNquadraticParallel(int N, int Dim, int size_row, int size_col, char* type
 		strcpy(fileName, path);
 		strcat(fileName, "/");
 		strcat(fileName, type);
-		strcat(fileName,"log_.txt");
+		strcat(fileName,"log_SUI.txt");
 		printf("%s\n", fileName);
 		file=fopen(fileName,"w");
 		fprintf(file, "N=%d, Dim=%d, rows=%d, cols=%d\n", N, Dim, size_row, size_col);
 		fprintf(file," Total time: %f\n", max_time);
-		double avg_time=0.0;
-		for(int i=0;i<N;i++){
-			fprintf(file," Total communication time for process %d: %f \n", i, *(comm_times+i));
-			avg_time+=*(comm_times+i);		
-		}	
-		avg_time=avg_time/N;
-		double min_time=min(comm_times, N);
-		double max_time=max(comm_times, N);
-		fprintf(file,"\n Average communication time: %f \n", avg_time);
-		fprintf(file," Minimal communication time: %f \n", min_time);
-		fprintf(file," Maximal communication time: %f \n", max_time);
 		fprintf(file," Required number of iterations: %d", iters);
 	
 		fclose(file);
 	}
 	free(X);
-	MPI_Finalize();
 }
 
-
-void printVector(double* vector, int n, char* name, int id){
-	printf("\n%d Printing %s\n", id, name);
-	int i;
-	for(i=0;i<n;++i){
-		printf("  %8.10f  ", vector[i]);
-	}
-	printf("\n");
-}
-
-double min(double* a, int Dim) {
-	double minVal = a[0];
-	int j;
-	for (j = Dim-1; j >0; --j)
-		minVal = MIN(minVal, a[j]);
-	return minVal;
-}
-
-int minInt(int* a, int Dim) {
-	int minVal = a[0];
-	int j;
-	for (j = Dim-1; j >0; --j)
-		minVal = MIN(minVal, a[j]);
-	return minVal;
-}
-
-double max(double* a, int Dim) {
-	double maxVal = a[0];
-	int j;
-	for (j = Dim-1; j >0; --j)
-		maxVal = MAX(maxVal, a[j]);
-	return maxVal;
-}
