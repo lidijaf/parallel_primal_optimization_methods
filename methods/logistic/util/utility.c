f213361e5c540fd97a7a8ddbb5e5beaa3ea75bfa
Lidija Fodor <lidija.fodor@dmi.uns.ac.rs>
2022-07-04 16:31:20 +0200

    Update utility.c

--- methods/logistic/util/utility.c
+++ methods/logistic/util/utility.c
@@ -23,3 +23,15 @@ double max(double* a, int Dim) {
 		maxVal = MAX(maxVal, a[j]);
 	return maxVal;
 }
+
+void find_optimal_distribution(int *N_f, int *rem, int size_row, int N){
+	if(size_row % N ==0){
+		*N_f=size_row/N;
+		*rem=0;
+	}
+	else{
+		*N_f=size_row / N;
+		*rem=size_row % N;
+	}
+
+}
