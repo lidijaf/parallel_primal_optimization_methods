8c1327b5cfe868db21ac62c9e07f604988ac1563
Lidija Fodor <lidija.fodor@dmi.uns.ac.rs>
2022-07-04 16:33:12 +0200

    Update neighbourhood_check.c

--- methods/logistic/neighborhood/neighbourhood_check.c
+++ methods/logistic/neighborhood/neighbourhood_check.c
@@ -1,1 +1,21 @@
+#include "neighbourhood_check.h"
 
+int is_my_neighbour(int my_rank, int i, int *my_neighbours, int my_neighbours_count){
+	for(int j=my_neighbours_count-1;j>=0;j--)
+		if(my_neighbours[j]==i)
+			return 1;
+	return 0;
+}
+
+int get_my_active_neighbour(int k, int my_rank, int *my_neighbours, int my_neighbours_count, int *active){
+	int cnt_active=-1;
+	int neighbour=-1;
+	for(int i=0;i<my_neighbours_count;i++){
+		if(active[my_neighbours[i]]){
+			++cnt_active;
+			neighbour=my_neighbours[i];
+		}
+		if(cnt_active==k)
+			return neighbour;
+	}
+}
